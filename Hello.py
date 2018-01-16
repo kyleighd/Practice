0015408df5b96e7b131e1d46d71517e891fd6511
Kyleigh <35408824+kyleighd@users.noreply.github.com>
2018-01-16 10:07:22 -0500

    Added more things to answer.

--- Hello.py
+++ Hello.py
@@ -2,3 +2,9 @@
 
 name = input("Hi, what's your name? ")
 print("Nice to meet you, " + name + "!")
+
+age = float(input("How old are you? ")
+if age > 50:
+print("You're too old! ")
+else:
+print("You're too young! ")
