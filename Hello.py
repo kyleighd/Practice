a42075d075947712fc9f40b48ac021095a3c915c
Kyleigh <35408824+kyleighd@users.noreply.github.com>
2018-01-17 00:25:14 -0500

    Fun with functions!

--- Hello.py
+++ Hello.py
@@ -1,10 +1,22 @@
 #!/bin/env python3
+# Author: Kyleigh
+# Date: 1/14/2018
 
-name = input("Hi, what's your name? ")
-print("Nice to meet you, " + name + "!")
+def get_name():
+  return input("What is your name? ")
 
-age = int(input("How old are you? "))
-if age > 50:
-  print("You're too old! ")
-else:
-  print("You're too young! ")
+def get_age():
+  return int(input("How old are you? "))
+  
+def print_stuff(name, age):
+  print("Nice to meet you, " + name + "!")
+  if age > 50:
+    print("You're too old!")
+  else:
+    print("You're too young!")
+    
+name = get_name()  
+age = get_age()
+print_stuff(name, age)
+
+
